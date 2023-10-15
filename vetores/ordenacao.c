bad242145f3126294d23d3472a94999538acfa72
Raí Salomão <123793652+raisalomao@users.noreply.github.com>
2023-10-15 14:03:30 -0300

    Add files via upload

---
+++ vetores/ordenacao.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+
+int main(){
+
+    int i, n, replace, vetor[3];
+
+    printf("Adicione 100 valores inteiros \n");
+
+    for (i = 0; i < 3; i++){
+        printf("Digite o numero %d: \n", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    for (i = 0; i < 3; i++){
+        for (n = i + 1; n < 3; n++){
+            if (vetor[i] > vetor[n]){
+                replace = vetor[i];
+                vetor[i] = vetor[n];
+                vetor[n] = replace;
+            }
+        }
+    }
+
+    printf("Em ordem crescente ficaria:\n");
+    for (i = 0; i < 3; i++){
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
